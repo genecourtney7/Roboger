fe07c9b6de9df32987e457ef7db615927fd3126a
gene courtney <genecourtney7@gmail.com>
2023-06-29 22:14:28 -0500

    It will produce an array to be used in the function.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//function roboger(string) {
+//     let inputArray = string.split(" ");
+//     let outputArray = [];
+
+//     for (let i = 0; i < wordArray.length; i++) {
+//         let currentWord = wordArray[i];
+//         let vowels = ['a', 'e', 'i', 'o', 'u'];
+//         let firstTwoLetters = currentWord.slice(0,2);
+//         let thirdLetters = currentWord.slice(2);
+//         let firstLetter = currentWord.slice(0, 1);
+//         let remainingLetters = currentWord.slice(1);
+
+//         if (vowels.indexOf(currentWord[0]) > -1) {
+//             vowelNewWord = currentWord + "way";
+//             outputArray.push(vowelNewWord);
+//         } else if (firstTwoLetters === "qu") {
+//             quNewWord = thirdLetters + firstTwoLetters + "ay";
+//             outputArray.push(quNewWord);
+//         } else {newWord = remainingLetters + firstLetter + "ay";
+//             outputArray.push(newWord);
+//         }; 
+//     } return outputArray.join(" ");
+// }
+
+let input = "input"
+
+Array.from({length: "input" + 1}, (v,i) => i);
