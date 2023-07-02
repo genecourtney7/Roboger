e9b03b60e51d7de92fbeafc7f9a8472832292989
gene courtney <genecourtney7@gmail.com>
2023-07-02 01:38:11 -0500

    added all functions, all should output properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,43 @@
-//function roboger(string) {
-//     let inputArray = string.split(" ");
-//     let outputArray = [];
-
-//     for (let i = 0; i < wordArray.length; i++) {
-//         let currentWord = wordArray[i];
-//         let vowels = ['a', 'e', 'i', 'o', 'u'];
-//         let firstTwoLetters = currentWord.slice(0,2);
-//         let thirdLetters = currentWord.slice(2);
-//         let firstLetter = currentWord.slice(0, 1);
-//         let remainingLetters = currentWord.slice(1);
-
-//         if (vowels.indexOf(currentWord[0]) > -1) {
-//             vowelNewWord = currentWord + "way";
-//             outputArray.push(vowelNewWord);
-//         } else if (firstTwoLetters === "qu") {
-//             quNewWord = thirdLetters + firstTwoLetters + "ay";
-//             outputArray.push(quNewWord);
-//         } else {newWord = remainingLetters + firstLetter + "ay";
-//             outputArray.push(newWord);
-//         }; 
-//     } return outputArray.join(" ");
-// }
-
-
-// const submitButton = document.querySelector('buttonSubmit')
-// submitButton.addEventListener("submit",numGen);
-
-
 function generateArrayFromNumberInput(input) {
     return Array.from({length:parseInt(input.value)+  1}, (v,i) => i);
 }
 
-   
+function replaceOnes(array) {
+    return array.map((value) =>  {
+        if(value.toString().includes("1")){
+            return "Beep!";
+        }  else  {
+            return value;
+        }
+    });
+}
 
+function replaceTwos(array){
+    return array.map((value) => {
+        if (value.toString().includes("2")) {
+            return"Boop!";
+        } else {
+            return value;
+        }
+    });
+}
 
+function replaceThrees(array){
+    return array.map((value) => {
+        if (value.toString().includes("3")) {
+            return"Won't you be my neighbor?";
+        } else {
+            return value;
+        }
+    });
+}
+
+function generateOutput(input){
+    const array = generateArrayFromNumberInput(input);
+    const onesReplaced = replaceOnes(array);
+    const twosReplaced = replaceTwos(onesReplaced);
+    const threesReplaced  = replaceThrees(twosReplaced);
+    const output = threesReplaced.join(",");
+
+    document.getElementById("array-output").textContent = output;
+}
